2a81c1b367b944d7a25d66da91879c2953efdbb8
arjun2309 <163283784+arjun2309@users.noreply.github.com>
2024-11-16 17:32:12 +0530

    Update WebConfig.java

--- src/main/java/com/project/config/WebConfig.java
+++ src/main/java/com/project/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**") .allowedOrigins("https://finance-tracker-s7bo.onrender.com") 
+		registry.addMapping("/**") .allowedOrigins("*") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  
                 .allowedHeaders("*")  
                 .allowCredentials(true);
