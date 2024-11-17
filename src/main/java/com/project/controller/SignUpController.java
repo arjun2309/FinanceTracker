b68bfb252d1b136e4e25d0911dcffcb70550bc64
arjun2309 <163283784+arjun2309@users.noreply.github.com>
2024-11-17 15:29:56 +0530

    Update SignUpController.java

--- src/main/java/com/project/controller/SignUpController.java
+++ src/main/java/com/project/controller/SignUpController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.dto.BudgetDto;
 import com.project.dto.ExpenseDto;
